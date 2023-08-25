3b0d6a3e3db3a156286827e19ec4c90ade201f0f
ashishrair500 <100395176+ashishrair500@users.noreply.github.com>
2023-08-25 23:11:55 +0530

    After this connecting frontend with backend

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -64,7 +64,15 @@ const deleteNote = (id)=>{
 // Edit a Note
 const editNote = (id, title, description, tag)=>{
 
+for(let index=0; index<notes.length; index++){
+const element=notes[index];
+if(element._id===id){
+  element.title=title;
+  element.description=description;
+  element.tag=tag;
+}
 
+}
 } 
     return (
     <NoteContext.Provider value={{notes,setNotes,addNote,deleteNote,editNote}}>          
