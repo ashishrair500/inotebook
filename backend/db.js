361216a927a1acd5135597984b45e9a975593918
ashishrair500 <100395176+ashishrair500@users.noreply.github.com>
2023-09-24 19:53:03 +0530

    migration to mongodb atlas from mongo compass

--- backend/db.js
+++ backend/db.js
@@ -2,12 +2,13 @@
 
 const mongoose = require('mongoose');  //like importing
 
-const mongoURI = "mongodb://127.0.0.1:27017/inotebook"
+//const mongoURI = "mongodb://127.0.0.1:27017/inotebook" //mongodb://localhost:3000
+const mongoURI = "mongodb+srv://ashish:111111As@cluster0.bf0shcd.mongodb.net/inotebook?retryWrites=true&w=majority" //mongodb://localhost:3000
 
 const connectToMongo = () => {                  //this function should in index js but to increase readbilit of our project we create separate component and 
                                                 //calling this function from index.js
     mongoose.connect(mongoURI);
-    console.log("connected to mongo succefully");
+    console.log("connected to mongo successfully");
 
 }
 
